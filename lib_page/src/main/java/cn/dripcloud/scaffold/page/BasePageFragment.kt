8c766b49d3b20d304620369656554b9ff9b88d5c
wangzhengfu <1797301908@139.com>
2022-01-15 23:34:18 +0800

    fragment 添加 onBackPressed 事件拦截

--- lib_page/src/main/java/cn/dripcloud/scaffold/page/BasePageFragment.kt
+++ lib_page/src/main/java/cn/dripcloud/scaffold/page/BasePageFragment.kt
@@ -1,27 +1,20 @@
 package cn.dripcloud.scaffold.page
 
-import android.app.Activity
-import android.content.Context
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 
 abstract class BasePageFragment<VB : ViewBinding, APPBAR: IAppBarView<out View>> : Fragment(), IBasePage<VB, APPBAR> {
-    lateinit var activity: Activity
     protected lateinit var binding: VB
     protected var appBarView: APPBAR? = null
     protected var pageStateView: IPageStateView? = null
     protected var isPageLoaded = false
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        activity = requireActivity()
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,25 +28,8 @@ abstract class BasePageFragment<VB : ViewBinding, APPBAR: IAppBarView<out View>>
             }
         }
         appBarView = onCreateAppBarView()
-        val pageDelegate = PageDelegate(activity, binding.root, appBarView, pageStateView)
+        val pageDelegate = PageDelegate(requireActivity(), binding.root, appBarView, pageStateView)
         return pageDelegate.rootView
-    }
-
-    override fun onResume() {
-        super.onResume()
-        // 配合 setMaxLifecycle 实现懒加载
-        if (!isPageLoaded && !isHidden) {
-            val lazyInit = {
-                onLazyInit()
-                isPageLoaded = true
-            }
-            Handler(Looper.getMainLooper()).postDelayed(lazyInit, 160)
-        }
-    }
-
-    private fun onLazyInit() {
-        doBusiness()
-        loadPageData()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -61,8 +37,41 @@ abstract class BasePageFragment<VB : ViewBinding, APPBAR: IAppBarView<out View>>
         initViewAndData(view, savedInstanceState)
     }
 
+    override fun onResume() {
+        super.onResume()
+        // 配合 setMaxLifecycle 实现懒加载
+        if (!isPageLoaded && !isHidden) {
+            val lazyInit = {
+                onLazyInitInternal()
+                isPageLoaded = true
+            }
+            Handler(Looper.getMainLooper()).postDelayed(lazyInit, 160)
+        }
+    }
+
+    private fun onLazyInitInternal() {
+        doBusiness()
+        loadPageData()
+        // 自定义返回导航
+        requireActivity().onBackPressedDispatcher.addCallback(this, mOnBackPressedCallback)
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         isPageLoaded = false
+    }
+
+    private val mOnBackPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            if (!onInterceptBackEvent()) {
+                isEnabled = false
+                requireActivity().onBackPressedDispatcher.onBackPressed()
+                isEnabled = true
+            }
+        }
+    }
+
+    open fun onInterceptBackEvent(): Boolean {
+        return false
     }
 }
