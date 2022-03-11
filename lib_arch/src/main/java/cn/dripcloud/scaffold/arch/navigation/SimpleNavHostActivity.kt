a3501dab7febd699619f0c0f70d2fa11dfd87d4a
wangzhengfu <1797301908@139.com>
2022-03-11 10:26:44 +0800

    添加 SimpleNavHostActivity

---
+++ lib_arch/src/main/java/cn/dripcloud/scaffold/arch/navigation/SimpleNavHostActivity.kt
@@ -0,0 +1,48 @@
+package cn.dripcloud.scaffold.arch.navigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment
+import cn.dripcloud.scaffold.arch.R
+import cn.dripcloud.scaffold.arch.databinding.LibArchNavigationContainerBinding
+import cn.dripcloud.scaffold.page.BasePageActivity
+import cn.dripcloud.scaffold.page.IPageStateView
+
+/**
+ * SimpleNavHostActivity
+ * @author wangzf
+ * @date 2022/3/11
+ */
+abstract class SimpleNavHostActivity : BasePageActivity<LibArchNavigationContainerBinding, View>() {
+
+    override fun onCreateViewBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        attachToRoot: Boolean
+    ): LibArchNavigationContainerBinding {
+        return LibArchNavigationContainerBinding.inflate(inflater)
+    }
+
+    override fun onPageViewCreated(savedInstanceState: Bundle?) {
+        val navHostFragment  = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navHostFragment.navController.apply {
+            val navGraph = navInflater.inflate(getNavGraphResId()).apply {
+                if (getCustomStartDestination() != 0) {
+                    startDestination = getCustomStartDestination()
+                }
+            }
+            setGraph(navGraph, getStartDestinationArgs())
+        }
+    }
+
+    override fun loadData() {}
+
+    abstract fun getNavGraphResId(): Int
+    open fun getCustomStartDestination() = 0
+    open fun getStartDestinationArgs(): Bundle? = null
+
+    override fun onCreateAppBarView(): View? = null
+    override fun onCreatePageStateView(): IPageStateView? = null
+}
