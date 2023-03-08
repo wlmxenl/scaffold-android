62a708b3c73356eaaa623910226ebe81e5e659ed
wangzf <1797301908@139.com>
2023-03-08 18:26:01 +0800

    列表分页 layoutmanager 示例

--- app/src/main/java/com/wlmxenl/scaffold/sample/features/paging/SamplePagingFragment.kt
+++ app/src/main/java/com/wlmxenl/scaffold/sample/features/paging/SamplePagingFragment.kt
@@ -1,12 +1,17 @@
 package com.wlmxenl.scaffold.sample.features.paging
 
 import android.os.Bundle
-import com.wlmxenl.scaffold.paging.ScaffoldPagingAdapter
-import com.wlmxenl.scaffold.paging.PagingExecutor
+import com.drake.brv.annotaion.DividerOrientation
+import com.drake.brv.utils.divider
+import com.drake.brv.utils.grid
 import com.drake.brv.utils.linear
+import com.drake.brv.utils.staggered
 import com.wlmxenl.scafflod.sample.R
-import com.wlmxenl.scaffold.sample.base.SampleBaseFragment
 import com.wlmxenl.scafflod.sample.databinding.PagingListLayoutBinding
+import com.wlmxenl.scaffold.navigation.scaffoldNavigate
+import com.wlmxenl.scaffold.paging.PagingExecutor
+import com.wlmxenl.scaffold.paging.ScaffoldPagingAdapter
+import com.wlmxenl.scaffold.sample.base.SampleBaseFragment
 
 /**
  *
@@ -15,15 +20,54 @@ import com.wlmxenl.scafflod.sample.databinding.PagingListLayoutBinding
  */
 class SamplePagingFragment : SampleBaseFragment<PagingListLayoutBinding>() {
     private lateinit var pagingExecutor: PagingExecutor<Any>
+    private val type by lazy {
+        arguments?.getInt("type") ?: R.id.linear
+    }
 
     override fun onPageViewCreated(savedInstanceState: Bundle?) {
         appBarView?.setup {
-            setTitle(R.string.sample_paging)
+            title = "${getString(R.string.sample_paging)} ${getTypeStr()}"
+            inflateMenu(R.menu.layoutmanager_options)
+            setOnMenuItemClickListener {
+                onLayoutManagerChanged(it.itemId)
+                true
+            }
         }
 
-        binding.rv.linear()
+        when (type) {
+            R.id.linear ->  {
+                binding.rv
+                    .divider {
+                        setDivider(1, true)
+                    }
+                    .linear()
+            }
+            R.id.grid -> {
+                binding.rv
+                    .divider {
+                        orientation = DividerOrientation.GRID
+                        includeVisible = true
+                        setDivider(10, true)
+                    }
+                    .grid(2)
+            }
+            R.id.staggered_grid -> {
+                binding.rv
+                    .divider {
+                        orientation = DividerOrientation.GRID
+                        includeVisible = true
+                        setDivider(10, true)
+                    }
+                    .staggered(2)
+            }
+        }
+
         val contentAdapter = ScaffoldPagingAdapter().apply {
-            addType<SamplePagingItem>(R.layout.paging_recycle_item)
+            when (type) {
+                R.id.linear -> addType<SamplePagingItem>(R.layout.paging_linear_recycle_item)
+                R.id.grid -> addType<SamplePagingItem>(R.layout.paging_grid_recycle_item)
+                R.id.staggered_grid -> addType<SamplePagingItem>(R.layout.paging_staggered_grid_recycle_item)
+            }
         }
 
         pagingExecutor = PagingExecutor.Builder<Any>()
@@ -35,5 +79,18 @@ class SamplePagingFragment : SampleBaseFragment<PagingListLayoutBinding>() {
 
     override fun onEnterAnimationEnd() {
         pagingExecutor.loadFirstPageData()
+    }
+
+    private fun onLayoutManagerChanged(id: Int) {
+        if (type == id) return
+        scaffoldNavigate(R.id.paging_fragment, Bundle().apply {
+            putInt("type", id)
+        })
+    }
+
+    private fun getTypeStr() = when (type) {
+        R.id.linear -> "linear"
+        R.id.grid -> "grid"
+        else -> "staggered_grid"
     }
 }
